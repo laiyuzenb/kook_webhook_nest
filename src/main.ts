fa00a18eac65de4bf6cb7de8503bf78ba6e53342
laiyuzenb <laiyuzenb@163.com>
2023-09-25 16:48:01 +0800

    提高body json大小限制

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { json } from 'express';
 import { AppModule } from 'src/app/app.module';
 import { get_config } from 'src/utils/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 提高body json 大小限制
+  app.use(json({ limit: '10mb' }));
 
   // 设置全局路由前缀
   app.setGlobalPrefix('api');
@@ -13,4 +16,5 @@ async function bootstrap() {
 
   await app.listen(port);
 }
+
 bootstrap();
